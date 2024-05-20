6b498aac7ae9f1f81968b35175895cca2995efcf
Austin Villamizar Medina <villamziarmedina@gmail.com>
2024-05-19 20:10:15 -0500

    update schema database

--- db/truequeando_db.sql
+++ db/truequeando_db.sql
@@ -1,13 +1,26 @@
-/*CREATE DATABASE IF EXISTS truequeando_db
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'Spanish_Spain.1252'
-    LC_CTYPE = 'Spanish_Spain.1252'
-    LOCALE_PROVIDER = 'libc'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;*/
+DO
+$$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_database
+      WHERE datname = 'truequeando_db'
+   ) THEN
+      PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE truequeando_db
+          WITH
+          OWNER = postgres
+          ENCODING = ''UTF8''
+          LC_COLLATE = ''Spanish_Spain.1252''
+          LC_CTYPE = ''Spanish_Spain.1252''
+          LOCALE_PROVIDER = ''libc''
+          TABLESPACE = pg_default
+          CONNECTION LIMIT = -1
+          IS_TEMPLATE = False');
+   END IF;
+END
+$$;
+
+-- Conectar a la base de datos truequeando_db
+\c truequeando_db
 
 CREATE TABLE IF NOT EXISTS rols (
     id SMALLSERIAL PRIMARY KEY NOT NULL CHECK (id >= 0),
@@ -67,4 +80,61 @@ CREATE TABLE IF NOT EXISTS city (
 	name VARCHAR NOT NULL,
 	states_id INT REFERENCES states(id) NOT NULL,
 	state BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS persons (
+	id SERIAL PRIMARY KEY NOT NULL,
+	number_identification INT NOT NULL CHECK (number_identification > 0) UNIQUE,
+	types_document_identification_id INT REFERENCES types_document_identification(id) NOT NULL,
+	photo BYTEA DEFAULT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	first_surname VARCHAR NOT NULL,
+	last_surname VARCHAR NOT NULL,
+	date_of_birth DATE NOT NULL,
+	-- nationatily
+	type_gener_id INT NOT NULL REFERENCES type_gener(id),
+	-- residence_city
+	email VARCHAR NOT NULL,
+	validated_email BOOLEAN NOT NULL DEFAULT FALSE,
+	number_phone INT DEFAULT NULL,
+	state BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE opinions (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR NOT NULL,
+	state BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE brands (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR NOT NULL,
+	state BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE type_products (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR NOT NULL,
+	state BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE type_states_products (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR NOT NULL,
+	state BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE products (
+	id SERIAL PRIMARY KEY NOT NULL,
+	name VARCHAR NOT NULL,
+	brand_id INT REFERENCES brands(id) NOT NULL,
+	seller_id INT REFERENCES persons(id) NOT NULL,
+	description VARCHAR NOT NULL,
+	price INT NOT NULL CHECK (price > 0),
+	quantity INT NOT NULL CHECK (quantity >= 0),
+	characteristics JSONB NOT NULL,
+	new BOOLEAN DEFAULT TRUE,
+	type_products_id INT REFERENCES type_products(id) NOT NULL,
+	type_states_products_id INT REFERENCES type_states_products(id) NOT NULL DEFAULT 1
 );
